156011575bdb706ba07427891041a7c86f33ba06
PrakharMittal-79 <117256177+PrakharMittal-79@users.noreply.github.com>
2024-06-03 23:11:22 +0530

    new-commit

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express=require('express');
 const mongoose= require('mongoose');
 const app=express();
@@ -7,11 +8,14 @@ const cookieParser=require('cookie-parser');
 const cors=require('cors');
 const BASE_URL=process.env.BASE_URL
 
-mongoose.connect('mongodb://127.0.0.1:27017/fasal_project')
+const db_URL=process.env.db_URL 
+
+mongoose.connect(db_URL)
 .then(()=>{
     console.log("DB Connected")
 })
-.catch(()=>{
+.catch((err)=>{
+    console.log(err)
     console.log("DB Connection failed");
 })
 
@@ -28,6 +32,6 @@ app.use(authRoutes);
 app.use(listRoutes);
 
 const PORT=process.env.PORT || 8080;
-app.listen(PORT,()=>{
+app.listen(8080,()=>{
     console.log('server is running on port 8080');
 })
